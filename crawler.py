1af8fa540969c24de9d1306305c672673853e391
CapitainCrunch <evstrat.bg@mail.ru>
2016-01-29 00:45:50 +0300

    Polina's part add

--- crawler.py
+++ crawler.py
@@ -2,7 +2,6 @@ import asyncio
 import aiohttp
 import re
 import requests
-import os
 
 
 urls = ['http://www.colta.ru/authors/346',
@@ -17,17 +16,9 @@ def collect_urls():
     for url in urls:
         page_content = requests.get(url).content.decode('utf8')
         article_url = re.findall(r'/articles/cinema/\d+', page_content, flags=re.DOTALL)
-        author_number = re.search('/([0-9]+)\\b', url)
-        if author_number is not None:
-                author = author_number.group(1)
         for i in set(article_url):
             article_urls.append(MAIN_URL + i)
-        article_info = (author, article_urls)
-        try:
-                os.mkdir(author)
-        except:
-                continue
-    return article_info
+    return article_urls
     
 
 @asyncio.coroutine
@@ -37,24 +28,28 @@ def get(*args, **kwargs):
 
 
 def parse_html(page):
-    # Полина часть
-    # TODO: Вытащить текст, тему и просмотры. Вернуть кортеж (тема, просмотры, текст, автор)
-    pass
+    article_name = re.findall('<section class="article.*?<h1>(.*?)</h1>', page, flags=re.DOTALL)
+    article_authors = re.findall('<a href="/authors/(\\d+)">(.*?)</a>', page, flags=re.DOTALL)
+    article_views = re.findall('<span class="view">(\\d+)</span>', page, flags=re.DOTALL)
+    article_content = re.findall('<div class="content">(.*?)<div class="share">', page, flags=re.DOTALL)
+    if article_content == []:
+        article_content = re.findall('<article class="content bigmat_content">.*?<p>(.*?)</article', page, flags=re.DOTALL)
+    name = ''.join(article_name[0].replace('\n', '').strip(' '))
+    views = article_views[0]
+    content = re.sub('<.*?>', '', article_content[0])
+    return (name, views, content, article_authors)
+
 
 
 @asyncio.coroutine
-def downlaod_html(url, author):
+def downlaod_html(url):
     with (yield from sem):
         page = yield from get(url)
         print(url)
-    articles = parse_html(page)
-
-    # Гуля
-    # TODO: Сохранить articles. Название файла - тема текста
-    topic = articles[0]
-    text =  articles[2]
-    article = open('./' + author + '/' + topic + '.txt', 'w', encoding='utf-8')
-    article.write(text)
+    article_data = parse_html(page)
+    print('./' + article_data[3][0][0][0] + '/' + article_data[0])
+    article = open('./' + article_data[0] + '.txt', 'w', encoding='utf-8')
+    article.write(article_data[2])
     article.close()
 
 # С помощью семафоров ограничиваем количество одновременных запросов (чтобы сайты не банили на пример)
