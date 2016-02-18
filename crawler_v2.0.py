9ca14a99a940ebea1016fbd13c29db0b611f6393
Angelina <aprisyazhnaya94@gmail.com>
2016-02-18 22:49:29 +0300

    Update crawler_v2.0.py

--- crawler_v2.0.py
+++ crawler_v2.0.py
@@ -2,9 +2,9 @@ import re
 import requests
 import os
 
-urls = ['http://www.colta.ru/authors/346',
-        'http://www.colta.ru/authors/26',
-        'http://www.colta.ru/authors/25']
+urls = ['http://www.colta.ru/authors/471',
+        'http://www.colta.ru/authors/66',
+        'http://www.colta.ru/authors/26']
 
 MAIN_URL = 'http://www.colta.ru'
 
@@ -12,7 +12,7 @@ MAIN_URL = 'http://www.colta.ru'
 def collect_urls(url):
     article_urls = []
     page_content = requests.get(url).content.decode('utf8')
-    article_url = re.findall(r'/articles/cinema/\d+', page_content, flags=re.DOTALL)
+    article_url = re.findall(r'/articles/\w+/\d+', page_content, flags=re.DOTALL)
     for i in set(article_url):
         article_urls.append(MAIN_URL + i)
     next_page = re.search(r'/authors/\d+\?page=\d+', page_content, flags=re.DOTALL)
@@ -30,10 +30,11 @@ def parse_html(page):
     article_content = re.findall('<div class="content">(.*?)<div class="share">', page, flags=re.DOTALL)
     if article_content == []:
         article_content = re.findall('<article class="content bigmat_content">.*?<p>(.*?)</article', page, flags=re.DOTALL)
-    name = ''.join(article_name[0].replace('\n', '').replace('/', '-').strip(' '))
-    views = article_views[0]
-    content = re.sub('<.*?>', '', article_content[0])
-    return [name, views, content, article_authors]
+    if article_name != []:    
+        name = ''.join(article_name[0].replace('\n', '').replace('/', '-').strip(' '))
+        views = article_views[0]
+        content = re.sub('<.*?>', '', article_content[0])
+        return [name, views, content, article_authors]
 
 
 for url in urls:
@@ -48,6 +49,12 @@ for url in urls:
     for article_url in article_urls:
         article_page = requests.get(article_url).content.decode('utf8')
         info = parse_html(article_page)
-        article = open('./' + author + '/' + info[0] + '.txt', 'w', encoding='utf-8')
-        article.write(info[2])
-        article.close()
+        if info is not None:
+            if info[0].endswith('»'):
+                continue
+            if len(info[2].split()) < 650:
+                continue
+            else:
+                article = open('./' + author + '/' + info[0] + '.txt', 'w', encoding='utf-8')
+                article.write(info[2])
+                article.close()
